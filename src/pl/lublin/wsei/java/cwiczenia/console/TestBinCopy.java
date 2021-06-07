af1028c9f7e4e09f23adb7832a825722e72e5003
deSp44 <michalcz97@gmail.com>
2021-06-07 11:44:21 +0200

    Zadanie nr.5

--- src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
+++ src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
@@ -1,5 +1,33 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
-public class TestBinCopy {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class TestBinCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("sample_1920x1280.tiff");
+            out = new FileOutputStream("img_copy.tiff");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        }
+        catch (IOException exception) {
+            System.out.println("IOException: " + exception.getMessage());
+            exception.printStackTrace();
+        }
+        finally {
+            if (in != null)
+                in.close();
+
+            if (out != null)
+                out.close();
+        }
+    }
 }
